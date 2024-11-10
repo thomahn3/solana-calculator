df2e065a6bbc4fd573f279cd11bdb985748d499a
Thomas <66449641+thomahn3@users.noreply.github.com>
2024-11-10 18:13:20 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## INFO
+I needed to multiple variables from my trades on the solana network that wasn't available from solscan or solana fm. This uses all public API's and doesn't require any private keys so should just be plug'n'play.
+
+## Functions
+1. Ask for transaction Signature
+2. Retrieve timestamp and token amount from SolanaFM [transaction API](https://docs.solana.fm/reference/get_transfers)
+3. Use [OHLC data](https://developers.binance.com/docs/binance-spot-api-docs/rest-api/public-rest-api-for-binance-2024-10-17) from BINCANCE public API to retrieve price of SOL/USDC since epoch time stamp.
+4. Calculate the average buy price of the token
+5. Displays the following:
+    - Time of transaction
+    - The price of SOL at the time of transaction
+    - The amount of SOL used to buy the coin
+    - The USD worth of SOL used to purchase the coin
+    - The amount of tokens recieved
+    - The total fees incurred during the transaction
+    - The average buy price of the coin (see limitations)
+
+
+## Limtitations
+Since its using SOL/USDC the average buy price isn't exact to USD but its pretty close and won't affect overall gains and losses too considerably. 
+
+## Installation
+1. Install dependencies
+```shell
+pip -r requirements.txt
+```
+2. Run calc.py
+
+---
+@thomahn3 :)
---
+++ calc.py
@@ -0,0 +1,52 @@
+import requests
+import json
+from time import strftime, localtime
+
+# Example signiture: 57oMGi1qoHynV6a9cXgyNuHaVTgLCPny5eEi255m793FyTAiZQs2R4B7QdWrgr6CCeo2G6eGxpFUqiEDYJ5NAK3E
+signiture = input("What is the transaction signiture? ")
+
+url = f'https://api.solana.fm/v0/transfers/{signiture}'
+
+headers = {"accept": "application/json"}
+
+response = requests.get(url, headers=headers)
+
+data = json.loads(response.text)
+
+for entry in data["result"]["data"]:
+    if entry["instructionIndex"] == 5 and entry["innerInstructionIndex"] == 1:
+        amounts = round(int(entry["amount"]) * 1e-6, 6) # Convert to actual amount of currency
+        timestamps = round(int(entry["timestamp"]) * 1e3) #Convert to epoch time with milliseconds\
+    if entry["instructionIndex"] == -1 and entry["innerInstructionIndex"] == -1:
+        gasFee = round(int(entry["amount"]) * 1e-9, 9)
+    if entry["instructionIndex"] == 8 and entry["innerInstructionIndex"] == -1:
+        prioFee = round(int(entry["amount"]) * 1e-9, 9)
+    if entry["instructionIndex"] == 5 and entry["innerInstructionIndex"] == 0:
+        solPurchase = round(int(entry["amount"]) * 1e-9, 9)
+
+time = strftime('%Y-%m-%d %H:%M:%S', localtime(timestamps*1e-3))
+
+# Convert timestamps to start and end
+url = f'https://api.binance.com/api/v3/klines?symbol=SOLUSDC&interval=1s&startTime={timestamps}&endTime={timestamps+999}'
+
+payload = {}
+headers = {
+  'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+    
+solPrice = float(json.loads(response.text)[0][1])
+
+totalFees = round(gasFee + prioFee, 9)
+solAmount = (solPurchase - totalFees)
+moneySpent = round((solPurchase * solPrice), 2)
+avgBuy = float((solPurchase * solPrice)/amounts)
+
+print("Timestamp of purchase:", time)
+print("Sol Price at purchase:", solPrice)
+print("Sol purchase amount:", solPurchase)
+print (f'Purchase Price: ${moneySpent}')
+print("Amount of tokens:", amounts)
+print("Fees:", totalFees)
+print("Average Buy Price: ", avgBuy)
